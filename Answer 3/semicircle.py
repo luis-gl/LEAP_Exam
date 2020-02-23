1defbff71442d3b095210f49f760f9d1daf934cd
luis-gl <42353343+luis-gl@users.noreply.github.com>
2020-02-22 23:52:28 -0500

    answer 3

---
+++ Answer 3/semicircle.py
@@ -0,0 +1,17 @@
+# Importing modules
+import math
+import numpy as np
+from random import uniform
+
+# Function to show n points of a semicircle
+def PrintPoints(cx, cy, r, n):
+    for i in np.arange(0, n):
+        # Select random angle
+        ang = uniform(0, 180)
+        # Calculate x and y components
+        x = r * math.cos(math.radians(ang)) + cx
+        y = r * math.sin(math.radians(ang)) + cy
+        print("(%f, %f)" %(x,y))
+
+# Function call
+PrintPoints(-1.5, 2, 3, 5)
