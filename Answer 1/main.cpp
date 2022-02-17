4191cda27c3d0dea8819ddd0e4a254d9e5ca6009
luis-gl <42353343+luis-gl@users.noreply.github.com>
2022-02-17 01:23:10 -0500

    added evidence of the new answer

---
+++ Answer 1/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <math.h>
+
+bool xyz(int n)
+{
+    float i = sqrt(n);
+    int j = ceil(i);
+    int k = 2;
+    int x = k;
+    while (x <= j)
+    {
+        if (!(n % x))
+            return false;
+        else
+            x++;
+    }
+    return  true;
+}
+
+int main()
+{
+    std::cout << "true is: " << true << "\n";
+    std::cout << "false is: " << false << "\n";
+    for (size_t i = 0; i < 51; i++)
+    {
+        if (xyz(i))
+            std::cout << i << "\n";
+    }
+    return 0;
+}
